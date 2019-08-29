fcc8fba38dd296a99d2a4c6cadc39f511e803037
Nick Smith <smithne@uw.edu>
2019-08-28 21:17:18 -0700

    Show messages using table in index. Add new message form

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,29 @@
 var express = require("express");
 var router = express.Router();
 
+// pre-populate data
+const messages = [
+  { text: "Hi there!", user: "Amando", added: new Date() },
+  { text: "Hello World!", user: "Charles", added: new Date() }
+];
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  const messages = [
-    { text: "Hi there!", user: "Amando", added: new Date() },
-    { text: "Hello World!", user: "Charles", added: new Date() }
-  ];
   res.render("index", { title: "Express", messages: messages });
 });
 
 router.get("/new", function(req, res, next) {
   res.render("new");
+});
+
+router.post("/new", function(req, res, next) {
+  const newMessage = {
+    text: req.body["message-text"],
+    user: req.body.user,
+    added: new Date()
+  };
+  messages.push(newMessage);
+  res.redirect("/");
 });
 
 module.exports = router;
